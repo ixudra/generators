a2ac49ccb1e97b2572b67452a89d4b5131007c11
Jan Oris <jan.oris@gmail.com>
2014-11-19 14:27:41 +0100

    Moved file paths to config

--- src/Ixudra/Generators/Commands/GenerateResourceCommand.php
+++ src/Ixudra/Generators/Commands/GenerateResourceCommand.php
@@ -3,8 +3,11 @@
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+
+use Config;
 
 class GenerateResourceCommand extends Command {
 
@@ -23,130 +26,12 @@ class GenerateResourceCommand extends Command {
     protected $variableSingular;
     protected $variablePlural;
 
-    protected $files;
-    protected $views;
-
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->files = array(
-            'model'                 => array(
-                'template'              => 'Model.txt',
-                'path'                  => app_path('models'),
-                'name'                  => '##VALUE##.php'
-            ),
-            'controller'            => array(
-                'template'              => 'Controller.txt',
-                'path'                  => app_path('controllers'),
-                'name'                  => '##VALUE##Controller.php'
-            ),
-            'controllerTest'        => array(
-                'template'              => 'ControllerTest.txt',
-                'path'                  => app_path('tests/unit/controllers'),
-                'name'                  => '##VALUE##ControllerTest.php'
-            ),
-            'repository'            => array(
-                'template'              => 'Repository.txt',
-                'path'                  => app_path('repositories/Eloquent'),
-                'name'                  => 'Eloquent##VALUE##Repository.php'
-            ),
-            'repositoryTest'        => array(
-                'template'              => 'RepositoryTest.txt',
-                'path'                  => app_path('tests/functional/repositories/Eloquent'),
-                'name'                  => 'Eloquent##VALUE##RepositoryTest.php'
-            ),
-            'Factory'               => array(
-                'template'              => 'Factory.txt',
-                'path'                  => app_path('services/creation'),
-                'name'                  => '##VALUE##Factory.php'
-            ),
-            'FactoryTest'           => array(
-                'template'              => 'FactoryTest.txt',
-                'path'                  => app_path('tests/functional/services/creation'),
-                'name'                  => '##VALUE##FactoryTest.php'
-            ),
-            'viewFactory'           => array(
-                'template'              => 'ViewFactory.txt',
-                'path'                  => app_path('services/presentation'),
-                'name'                  => '##VALUE##ViewFactory.php'
-            ),
-            'viewFactoryTest'       => array(
-                'template'              => 'ViewFactoryTest.txt',
-                'path'                  => app_path('tests/unit/services/presentation'),
-                'name'                  => '##VALUE##ViewFactoryTest.php'
-            ),
-            'inputHelper'           => array(
-                'template'              => 'InputHelper.txt',
-                'path'                  => app_path('services/input'),
-                'name'                  => '##VALUE##InputHelper.php'
-            ),
-            'formHelper'            => array(
-                'template'              => 'FormHelper.txt',
-                'path'                  => app_path('services/form'),
-                'name'                  => '##VALUE##FormHelper.php'
-            ),
-            'formHelperTest'        => array(
-                'template'              => 'FormHelperTest.txt',
-                'path'                  => app_path('tests/unit/services/form'),
-                'name'                  => '##VALUE##FormHelperTest.php'
-            ),
-            'validator'             => array(
-                'template'              => 'Validator.txt',
-                'path'                  => app_path('services/validation'),
-                'name'                  => '##VALUE##Validator.php'
-            ),
-            'validatorTest'         => array(
-                'template'              => 'ValidatorTest.txt',
-                'path'                  => app_path('tests/unit/services/validation'),
-                'name'                  => '##VALUE##ValidatorTest.php'
-            ),
-            'presenter'             => array(
-                'template'              => 'Presenter.txt',
-                'path'                  => app_path('presenters'),
-                'name'                  => '##VALUE##Presenter.php'
-            )
-        );
-
-        $this->views = array(
-            'indexView'             => array(
-                'template'              => 'IndexView.txt',
-                'name'                  => 'index.blade.php'
-            ),
-            'listView'             => array(
-                'template'              => 'ListView.txt',
-                'name'                  => 'list.blade.php'
-            ),
-            'tableView'             => array(
-                'template'              => 'TableView.txt',
-                'name'                  => 'table.blade.php'
-            ),
-            'showView'             => array(
-                'template'              => 'ShowView.txt',
-                'name'                  => 'show.blade.php'
-            ),
-            'createView'             => array(
-                'template'              => 'CreateView.txt',
-                'name'                  => 'create.blade.php'
-            ),
-            'editView'             => array(
-                'template'              => 'EditView.txt',
-                'name'                  => 'edit.blade.php'
-            ),
-            'formView'             => array(
-                'template'              => 'FormView.txt',
-                'name'                  => 'form.blade.php'
-            ),
-        );
-    }
-
 
     public function fire()
     {
         $this->deriveArguments();
 
-        foreach( $this->files as $file ) {
+        foreach( Config::get("generators::config.files") as $file ) {
             $template = $this->loadTemplate($file['template']);
             $content = $this->replaceValues( $template );
             $this->createFile( $file['name'], $file['path'], $content );
@@ -154,7 +39,7 @@ class GenerateResourceCommand extends Command {
 
         $path = app_path('views') . '/bootstrap/' . $this->variablePlural;
         $this->createDirectory($path);
-        foreach( $this->views as $view ) {
+        foreach( Config::get("generators::config.views") as $view ) {
             $template = $this->loadTemplate($view['template']);
             $content = $this->replaceValues( $template );
             $this->createFile( $view['name'], $path, $content );
---
+++ src/config/config.php
@@ -0,0 +1,137 @@
+<?php
+
+
+    return array(
+
+        'files'     => array(
+
+            'model'                 => array(
+                'template'              => 'Model.txt',
+                'path'                  => app_path('models'),
+                'name'                  => '##VALUE##.php'
+            ),
+
+            'controller'            => array(
+                'template'              => 'Controller.txt',
+                'path'                  => app_path('controllers'),
+                'name'                  => '##VALUE##Controller.php'
+            ),
+
+            'controllerTest'        => array(
+                'template'              => 'ControllerTest.txt',
+                'path'                  => app_path('tests/unit/controllers'),
+                'name'                  => '##VALUE##ControllerTest.php'
+            ),
+
+            'repository'            => array(
+                'template'              => 'Repository.txt',
+                'path'                  => app_path('repositories/Eloquent'),
+                'name'                  => 'Eloquent##VALUE##Repository.php'
+            ),
+
+            'repositoryTest'        => array(
+                'template'              => 'RepositoryTest.txt',
+                'path'                  => app_path('tests/functional/repositories/Eloquent'),
+                'name'                  => 'Eloquent##VALUE##RepositoryTest.php'
+            ),
+
+            'Factory'               => array(
+                'template'              => 'Factory.txt',
+                'path'                  => app_path('services/creation'),
+                'name'                  => '##VALUE##Factory.php'
+            ),
+
+            'FactoryTest'           => array(
+                'template'              => 'FactoryTest.txt',
+                'path'                  => app_path('tests/functional/services/creation'),
+                'name'                  => '##VALUE##FactoryTest.php'
+            ),
+
+            'viewFactory'           => array(
+                'template'              => 'ViewFactory.txt',
+                'path'                  => app_path('services/presentation'),
+                'name'                  => '##VALUE##ViewFactory.php'
+            ),
+
+            'viewFactoryTest'       => array(
+                'template'              => 'ViewFactoryTest.txt',
+                'path'                  => app_path('tests/unit/services/presentation'),
+                'name'                  => '##VALUE##ViewFactoryTest.php'
+            ),
+
+            'inputHelper'           => array(
+                'template'              => 'InputHelper.txt',
+                'path'                  => app_path('services/input'),
+                'name'                  => '##VALUE##InputHelper.php'
+            ),
+
+            'formHelper'            => array(
+                'template'              => 'FormHelper.txt',
+                'path'                  => app_path('services/form'),
+                'name'                  => '##VALUE##FormHelper.php'
+            ),
+
+            'formHelperTest'        => array(
+                'template'              => 'FormHelperTest.txt',
+                'path'                  => app_path('tests/unit/services/form'),
+                'name'                  => '##VALUE##FormHelperTest.php'
+            ),
+
+            'validator'             => array(
+                'template'              => 'Validator.txt',
+                'path'                  => app_path('services/validation'),
+                'name'                  => '##VALUE##Validator.php'
+            ),
+
+            'validatorTest'         => array(
+                'template'              => 'ValidatorTest.txt',
+                'path'                  => app_path('tests/unit/services/validation'),
+                'name'                  => '##VALUE##ValidatorTest.php'
+            ),
+
+            'presenter'             => array(
+                'template'              => 'Presenter.txt',
+                'path'                  => app_path('presenters'),
+                'name'                  => '##VALUE##Presenter.php'
+            )
+        ),
+
+        'views'         => array(
+
+            'indexView'             => array(
+                'template'              => 'IndexView.txt',
+                'name'                  => 'index.blade.php'
+            ),
+
+            'listView'             => array(
+                'template'              => 'ListView.txt',
+                'name'                  => 'list.blade.php'
+            ),
+
+            'tableView'             => array(
+                'template'              => 'TableView.txt',
+                'name'                  => 'table.blade.php'
+            ),
+
+            'showView'             => array(
+                'template'              => 'ShowView.txt',
+                'name'                  => 'show.blade.php'
+            ),
+
+            'createView'             => array(
+                'template'              => 'CreateView.txt',
+                'name'                  => 'create.blade.php'
+            ),
+
+            'editView'             => array(
+                'template'              => 'EditView.txt',
+                'name'                  => 'edit.blade.php'
+            ),
+
+            'formView'             => array(
+                'template'              => 'FormView.txt',
+                'name'                  => 'form.blade.php'
+            ),
+        )
+
+    );
